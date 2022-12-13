c99809bf7fc0cb9e9dfb42ee7ded951e021fcaa1
himanshu_red <himanshutdw98@gmail.com>
2022-12-13 14:59:10 +0530

    injective literal values

--- spring_demo_one/src/com/himanshu/springdemo/CricketCoach.java
+++ spring_demo_one/src/com/himanshu/springdemo/CricketCoach.java
@@ -3,19 +3,38 @@ package com.himanshu.springdemo;
 public class CricketCoach implements Coach {
 	
 	private FortuneService fortuneService; 
+	private String emailAddress; 
+	private String team; 
 	
 	public CricketCoach() {
 		System.out.println("CricketCoach : Inside No-args constructor invoked");
 	}
 	
-	
 	// setter method
 	public void setFortuneService(FortuneService fortuneService) {
 		this.fortuneService = fortuneService;
-		System.out.println("CricketCoach: Inside setter method");
+		System.out.println("CricketCoach: Inside set fortune service method");
 	}
 
+	public String getEmailAddress() {
+		System.out.println("Inside get email address method");
+		return emailAddress;
+	}
 
+	public void setEmailAddress(String emailAddress) {
+		System.out.println("Inside set email address method");
+		this.emailAddress = emailAddress;
+	}
+
+	public String getTeam() {
+		System.out.println("Inside get team address method");
+		return team;
+	}
+
+	public void setTeam(String team) {
+		System.out.println("Inside set team address method");
+		this.team = team;
+	}
 
 	@Override
 	public String getDailyWorkout() {
@@ -27,4 +46,5 @@ public class CricketCoach implements Coach {
 		fortuneService.setFortune("You'll hit 6 sixes today"); 
 		return fortuneService.getFortune();
 	}
+	
 }
