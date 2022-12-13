44009be147f4295fa415ae1902516f22f3ff955f
himanshu_red <himanshutdw98@gmail.com>
2022-12-13 14:03:38 +0530

    setter injection

--- spring_demo_one/src/com/himanshu/springdemo/Coach.java
+++ spring_demo_one/src/com/himanshu/springdemo/Coach.java
@@ -3,6 +3,6 @@ package com.himanshu.springdemo;
 public interface Coach {
 
 	public String getDailyWorkout() ;
-	
+	public void setFortuneService(FortuneService fortuneService); 
 	public String getDailyFortune(); 
 }
--- spring_demo_one/src/com/himanshu/springdemo/CricketCoach.java
+++ spring_demo_one/src/com/himanshu/springdemo/CricketCoach.java
@@ -1,18 +1,26 @@
 package com.himanshu.springdemo;
 
-public class CricketCoach implements Coach{
+public class CricketCoach implements Coach {
 	
 	private FortuneService fortuneService; 
 	
-	public CricketCoach(FortuneService fortuneService) {
-		this.fortuneService = fortuneService; 
+	public CricketCoach() {
+		System.out.println("CricketCoach : Inside No-args constructor invoked");
 	}
 	
-	@Override
-	public String getDailyWorkout() {
-		return "Bring the bats and wickets."; 
+	
+	// setter method
+	public void setFortuneService(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+		System.out.println("CricketCoach: Inside setter method");
 	}
 
+
+
+	@Override
+	public String getDailyWorkout() {
+		return "practice fast bowling for 1 hour a day"; 
+	}
 	
 	@Override
 	public String getDailyFortune() {
